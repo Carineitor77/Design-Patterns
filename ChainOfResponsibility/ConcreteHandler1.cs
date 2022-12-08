7ce761e7827f6d5ab2dab610bd8d88acec16f300
Anatoly <algash04@gmail.com>
2022-12-09 00:11:39 +0200

    Implemented the chain of responsibility pattern

---
+++ ChainOfResponsibility/ConcreteHandler1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    public class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleRequest(request);
+            }
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ChainOfResponsibility
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Handler h1 = new ConcreteHandler1();
+            Handler h2 = new ConcreteHandler2();
+
+            h1.Successor = h2;
+            h1.HandleRequest(1);
+            h1.HandleRequest(2);
+        }
+    }
+}
