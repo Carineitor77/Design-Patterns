96542ab7436e3aa58c05838d6d9d37b56ce6ddb0
Anatoly <algash04@gmail.com>
2022-12-09 23:27:07 +0200

    Implemented the interpreter pattern

---
+++ Interpreter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interpreter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Context context = new Context
+            {
+                Vocabulary = 'a',
+                Source = "aaa"
+            };
+
+            var expression = new NonterminalExpression();
+            expression.Interpret(context);
+
+            Console.WriteLine(context.Result);
+        }
+    }
+}
---
+++ Interpreter/TerminalExpression.cs
@@ -0,0 +1,10 @@
+﻿namespace Interpreter
+{
+    public class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            context.Result = context.Source[context.Position] == context.Vocabulary;
+        }
+    }
+}
