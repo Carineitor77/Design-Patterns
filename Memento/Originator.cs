8ff22a537daf4185662099a8a2c32bf2e8d8e923
Anatoly <algash04@gmail.com>
2022-12-10 13:19:13 +0200

    Implemented the memento pattern

---
+++ Memento/Originator.cs
@@ -0,0 +1,17 @@
+﻿namespace Memento
+{
+    public class Originator
+    {
+        public string State { get; set; }
+
+        public void SetMemento(Memento memento)
+        {
+            State = memento.State;
+        }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(State);
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Memento
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Originator originator = new Originator();
+            originator.State = "On";
+
+            Caretaker caretaker = new Caretaker();
+            caretaker.Memento = originator.CreateMemento();
+
+            originator.State = "Off";
+
+            originator.SetMemento(caretaker.Memento);
+        }
+    }
+}
