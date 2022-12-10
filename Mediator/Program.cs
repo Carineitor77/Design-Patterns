868e8431ac53ce04493b1474001875c6c60b06e2
Anatoly <algash04@gmail.com>
2022-12-10 12:40:28 +0200

    Implemented the mediator pattern

---
+++ Mediator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Mediator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var mediator = new ConcreteMediator();
+            var colleague1 = new ConcreteColleague1(mediator);
+            var colleague2 = new ConcreteColleague2(mediator);
+
+            mediator.Colleague1 = colleague1;
+            mediator.Colleague2 = colleague2;
+            colleague1.Send("Message 1");
+            colleague2.Send("Message 2");
+        }
+    }
+}
